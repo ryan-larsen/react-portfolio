72a5c798c32c2cc9442f28ed72736cfe006d2390
Ryan <larsenr12@gmail.com>
2020-04-11 19:55:04 -0400

    Changed folders ts to fix github push and commit errors

---
+++ src/resumeData.js
@@ -0,0 +1,95 @@
+const resumeData = {
+  imagebaseurl: 'https://ryan-larsen.github.io/',
+  name: 'Ryan Larsen',
+  role: 'Full Stack Web Developer',
+  roleDescription: 'I am a full stack web developer with experience previously as a network administrator',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/ryan-larsen/',
+      className: 'fa fa-linkedin'
+    },
+    {
+      name: 'github',
+      url: 'http://github.com/ryan-larsen',
+      className: 'fa fa-github'
+    }
+  ],
+  aboutme: 'I am currently a Network Administrator at Hyland Software and I am looking to pursue a career in development',
+  address: 'Cleveland, Ohio',
+  website: 'https://ryan-larsen.com',
+  education: [
+    {
+      UniversityName: 'The University of Akron',
+      specialization: 'Computer Information Systems - Networking'
+    }
+  ],
+  work: [
+    {
+      CompanyName: 'Hyland Software',
+      specialization: 'Network Administrator',
+      MonthOfLeaving: 'Current',
+      YearOfLeaving: 'Current'
+    },
+    {
+      CompanyName: 'North Olmsted City Schools',
+      specialization: 'Help Desk/Jr. Network Administrator',
+      MonthOfLeaving: 'March',
+      YearOfLeaving: '2020'
+    }
+  ],
+  skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'HTML5'
+    },
+    {
+      skillname: 'CSS'
+    },
+    {
+      skillname: 'Bootstrap'
+    },
+    {
+      skillname: 'Node.js'
+    },
+    {
+      skillname: 'Express.js'
+    },
+    {
+      skillname: 'JavaScript'
+    },
+    {
+      skillname: 'SQL'
+    },
+    {
+      skillname: 'mongoDb'
+    },
+    {
+      skillname: 'React'
+    }
+  ],
+  portfolio: [
+    {
+      name: 'Burger-App',
+      description: 'App using handlebars, node, and sql to post and get items from a database ',
+      imgurl: 'images/burgerapp.png'
+    },
+    {
+      name: 'Budget-app',
+      description: 'Budget tracking app that uses react with a mongo database.',
+      imgurl: 'images/budget.png'
+    },
+    {
+      name: 'Notetaking-app',
+      description: 'Note taking app that uses express and node.',
+      imgurl: 'images/note-app.png'
+    },
+    {
+      name: 'Flight Vacation App',
+      description: 'Vacation app that uses html/css/javascript to determine weather for ',
+      imgurl: 'images/flight-app.png'
+    }
+  ]
+}
+
+export default resumeData
